e911e9d60bb092973b017948837a0cbd97cc93b3
andreinalivaika <nav.testsw@gmail.com>
2023-03-23 18:11:22 +0300

    4 add styled-components lib

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,25 +1,36 @@
-import logo from '../../assets/logo.svg'
-import './App.css'
+import styled from 'styled-components'
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledApp>
+      <div className="App">Learn React</div>
+    </StyledApp>
   )
 }
 
-export default App
+const StyledApp = styled.div`
+  text-align: center;
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+
+  @media (prefers-reduced-motion: no-preference) {
+    .App-logo {
+      animation: App-logo-spin infinite 20s linear;
+    }
+  }
+
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`
