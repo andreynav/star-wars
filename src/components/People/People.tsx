62bad5a13ef50bae537dfb25dcd9840dda61292b
andreinalivaika <nav.testsw@gmail.com>
2023-03-29 16:49:43 +0300

    17 fiat: add person info to component

--- src/components/People/People.tsx
+++ src/components/People/People.tsx
@@ -52,6 +52,60 @@ export const People = ({ error, setError }: any) => {
                   <div>
                     <b>Title:</b> {person.name}
                   </div>
+                  <div>
+                    <b>Gender:</b> {person.gender}
+                  </div>
+                  <div>
+                    <b>Birth year:</b> {person.birth_year}
+                  </div>
+                  <div>
+                    <b>Eye color:</b> {person.eye_color}
+                  </div>
+                  <div>
+                    <b>Hair color:</b> {person.hair_color}
+                  </div>
+                  <div>
+                    <b>Height:</b> {person.height}
+                  </div>
+                  <div>
+                    <b>Mass:</b> {person.mass}
+                  </div>
+                  <div>
+                    <b>Skin color:</b> {person.skin_color}
+                  </div>
+                  <div>
+                    <b>Home world:</b> {person.homeworld}
+                  </div>
+                  <div>
+                    <b>Films:</b>{' '}
+                    <a href={person.films[0]} target="_blank" rel="noopener noreferrer">
+                      {person.films[0]}
+                    </a>
+                  </div>
+                  <div>
+                    <b>Species:</b>{' '}
+                    <a href={person.species[0]} target="_blank" rel="noopener noreferrer">
+                      {person.species[0]}
+                    </a>
+                  </div>
+                  <div>
+                    <b>Starships:</b>{' '}
+                    <a href={person.starships[0]} target="_blank" rel="noopener noreferrer">
+                      {person.starships[0]}
+                    </a>
+                  </div>
+                  <div>
+                    <b>Vehicles:</b>{' '}
+                    <a href={person.vehicles[0]} target="_blank" rel="noopener noreferrer">
+                      {person.vehicles[0]}
+                    </a>
+                  </div>
+                  <div>
+                    <b>Url:</b>{' '}
+                    <a href={person.url} target="_blank" rel="noopener noreferrer">
+                      {person.url}
+                    </a>
+                  </div>
                 </CardContainer>
               )
             })}
