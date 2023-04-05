5994b59c967762e9b8e22f7a53ed0ad561c6990a
andreinalivaika <nav.testsw@gmail.com>
2023-04-05 17:53:06 +0300

    31 feat: add DetailedCardSection section collapse

--- src/components/DetailedCardSection/DetailedCardSection.tsx
+++ src/components/DetailedCardSection/DetailedCardSection.tsx
@@ -1,32 +1,51 @@
+import { useState } from 'react'
 import styled from 'styled-components'
 
 import { imageBaseApi } from '../../api/api'
+import { DetailedCardSectionT } from '../../types/types'
 import { getCategoryFromUrl } from '../../utils/getCategoryFromUrl'
 import { getIdFromUrl } from '../../utils/getIdFromUrl'
 import { Card } from '../Card/Card'
+import { ShowMore } from '../ShowMore/ShowMore'
 
-export const DetailedCardSection = ({ title, data }: any) => {
-  const thumbnails = (
-    <ThumbnailContainer>
-      {data.map((item: string) => {
-        return (
-          <SectionItem key={item}>
-            <Card
-              category={getCategoryFromUrl(item)!}
-              toNavigate={`/${getCategoryFromUrl(item)}/${getIdFromUrl(item)}`}
-              src={`${imageBaseApi}${getCategoryFromUrl(item)}/${getIdFromUrl(item)}.jpg`}
-            />
-          </SectionItem>
-        )
-      })}
-    </ThumbnailContainer>
-  )
+export const DetailedCardSection = ({ title, data }: DetailedCardSectionT) => {
+  const [isShowMoreVisible, setIsShowMoreVisible] = useState(data.length > 3)
+  const toggleShowMore = () => {
+    setIsShowMoreVisible(!isShowMoreVisible)
+  }
+
+  const getThumbnails = (data: any, sliceArgs: any) => {
+    return (
+      <ThumbnailContainer>
+        {data.slice(...sliceArgs).map((item: string) => {
+          return (
+            <SectionItem key={item}>
+              <Card
+                category={getCategoryFromUrl(item)!}
+                toNavigate={`/${getCategoryFromUrl(item)}/${getIdFromUrl(item)}`}
+                src={`${imageBaseApi}${getCategoryFromUrl(item)}/${getIdFromUrl(item)}.jpg`}
+              />
+            </SectionItem>
+          )
+        })}
+      </ThumbnailContainer>
+    )
+  }
 
   return (
     <Section>
-      <div>{title}</div>
+      <SectionTitle>{title}</SectionTitle>
       {data.length > 0 ? (
-        thumbnails
+        <>
+          <TopContainer>
+            {getThumbnails(data, [0, 3])}
+            {isShowMoreVisible && <ShowMore onClick={toggleShowMore} nameIsShow />}
+          </TopContainer>
+          <BottomContainer>
+            {!isShowMoreVisible && getThumbnails(data, [3])}
+            {!isShowMoreVisible && data.length !== 3 && <ShowMore onClick={toggleShowMore} />}
+          </BottomContainer>
+        </>
       ) : (
         <SectionItem>There is no data in the Jedi archives</SectionItem>
       )}
@@ -37,11 +56,20 @@ export const DetailedCardSection = ({ title, data }: any) => {
 const Section = styled.div`
   display: grid;
   align-content: start;
+`
 
-  & div:nth-child(1) {
-    font-weight: bold;
-    line-height: 2rem;
-  }
+const SectionTitle = styled.div`
+  font-weight: bold;
+`
+
+const TopContainer = styled.div`
+  display: grid;
+  grid-row-gap: 1rem;
+`
+
+const BottomContainer = styled.div`
+  display: grid;
+  grid-row-gap: 1rem;
 `
 
 const SectionItem = styled.div`
@@ -51,5 +79,6 @@ const SectionItem = styled.div`
 const ThumbnailContainer = styled.div`
   display: grid;
   grid-gap: 1rem;
+  padding-top: 1rem;
   grid-template-columns: repeat(3, 1fr);
 `
