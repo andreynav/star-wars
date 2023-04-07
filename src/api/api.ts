8fae747cb3af9ad1ecbcefa77424b59036c4251a
andreinalivaika <nav.testsw@gmail.com>
2023-04-07 15:26:02 +0300

    33 feat: add species api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-import { FilmT, FilmsListT, PeopleListT, PersonT } from '../types/types'
+import { FilmT, FilmsListT, PeopleListT, PersonT, SpeciesListT, SpeciesOneT } from '../types/types'
 
 const swapi = axios.create({
   baseURL: 'https://swapi.dev/api/',
@@ -19,6 +19,12 @@ export const swAPI = {
   },
   getPerson: (id: string) => {
     return swapi.get<PersonT>(`people/${id}/`).then((response) => response.data)
+  },
+  getAllSpecies: (pageNumber = 1) => {
+    return swapi.get<SpeciesListT>(`species/?page=${pageNumber}`).then((response) => response.data)
+  },
+  getSpeciesOne: (id: string) => {
+    return swapi.get<SpeciesOneT>(`species/${id}/`).then((response) => response.data)
   },
   getAllFilms: () => {
     return swapi.get<FilmsListT>(`films/`).then((response) => response.data)
--- src/types/types.ts
+++ src/types/types.ts
@@ -49,6 +49,31 @@ export type PeopleListT = {
   results: PersonT[]
 }
 
+export type SpeciesOneT = {
+  name: string
+  classification: string
+  designation: string
+  average_height: string
+  average_lifespan: string
+  eye_colors: string
+  hair_colors: string
+  skin_colors: string
+  language: string
+  homeworld: string
+  people: string[]
+  films: string[]
+  url: string
+  created: string
+  edited: string
+}
+
+export type SpeciesListT = {
+  count: number
+  next: string
+  previous: string
+  results: SpeciesOneT[]
+}
+
 export type StyledToggleT = {
   width?: number
   height?: number
