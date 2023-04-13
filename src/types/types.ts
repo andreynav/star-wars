2a25f0e486f5c59b401fc6109d40ea5b2aa47cfa
andreinalivaika <nav.testsw@gmail.com>
2023-04-13 18:16:02 +0300

    48 feat: add detailed starships page

--- src/types/types.ts
+++ src/types/types.ts
@@ -52,7 +52,23 @@ export type PlanetsT = {
   films: string[]
 } & CommonSectionPropsT
 
-export type StarshipsT = {} & CommonSectionPropsT
+export type StarshipsT = {
+  name: string
+  model: string
+  starship_class: string
+  manufacturer: string
+  cost_in_credits: string
+  length: string
+  crew: string
+  passengers: string
+  max_atmosphering_speed: string
+  hyperdrive_rating: string
+  MGLT: string
+  cargo_capacity: string
+  consumables: string
+  films: string[]
+  pilots: string[]
+} & CommonSectionPropsT
 
 export type VehiclesT = {} & CommonSectionPropsT
 
