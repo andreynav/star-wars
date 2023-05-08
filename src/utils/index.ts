11e8ba8eb8480d9428ec94ee3ba4b868292652c8
andreinalivaika <nav.testsw@gmail.com>
2023-05-08 18:29:18 +0300

    82 ref: delete redundant getNameIndexShift

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,13 +3,5 @@ import { getBottomData } from './getBottomData'
 import { getCategoryFromUrl } from './getCategoryFromUrl'
 import { getIdFromUrl } from './getIdFromUrl'
 import { getImgPathCategory } from './getImgPathCategory'
-import { getNameIndexShift } from './getNameIndexShift'
 
-export {
-  convertToTitleCase,
-  getBottomData,
-  getCategoryFromUrl,
-  getIdFromUrl,
-  getImgPathCategory,
-  getNameIndexShift
-}
+export { convertToTitleCase, getBottomData, getCategoryFromUrl, getIdFromUrl, getImgPathCategory }
